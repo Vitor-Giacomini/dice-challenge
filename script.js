1a03702fb09b2df42deace3147c4211d95349480
Vitor-Giacomini <vitorgdgiacomini@gmail.com>
2023-11-04 18:43:08 -0300

    add scripts to new buttons

--- script.js
+++ script.js
@@ -2,63 +2,95 @@ import { Point } from "./models/point.js";
 import { Side } from "./models/side.js";
 
 let currentDice = [];
+let sideSize = 50;
 
 document.addEventListener("DOMContentLoaded", function () {
-  const translateLeftButton = document.getElementById('translate-left');
-  if(translateLeftButton){
-    translateLeftButton.addEventListener('click', translateLeft);
-  }
-  const translateDownButton = document.getElementById('translate-down');
-  if(translateDownButton){
-    translateDownButton.addEventListener('click', translateDown);
-  }
+  setupButtons();
 
-  // Roda ao iniciar o documento, monta o dado em seu primeiro estado
   const dice = [
     new Side([new Point(50, 50), new Point(50, -50), new Point(-50, -50), new Point(-50, 50), new Point(50, 50)], 1),
-    new Side([new Point(50, -150), new Point(50, -150), new Point(-50, -150), new Point(-50, -50), new Point(50, -50)], 2),
+    new Side([new Point(150, 50), new Point(150, -50), new Point(50, -50), new Point(50, 50), new Point(150, 50)], 2),
     new Side([new Point(50, 150), new Point(50, 50), new Point(-50, 50), new Point(-50, 150), new Point(50, 150)], 3),
-    new Side([new Point(150, 50), new Point(150, -50), new Point(50, -50), new Point(50, 50), new Point(150, 50)], 4),
-    new Side([new Point(-150, 50), new Point(-150, -50), new Point(-50, -50), new Point(-50, 50), new Point(50, 50)], 5),
+    new Side([new Point(50, -50), new Point(50, -150), new Point(-50, -150), new Point(-50, -50), new Point(50, -50)], 4),
+    new Side([new Point(-50, 50), new Point(-50, -50), new Point(-150, -50), new Point(-150, 50), new Point(-50, 50)], 5),
     new Side([new Point(-250, 50), new Point(-250, -50), new Point(-150, -50), new Point(-150, 50), new Point(-150, 50)], 6)
   ]
   draw(dice);
 });
 
-function translateLeft() {
-  console.log('translated');
+function translate(xTranslation, yTranslation) {
   const newDice = currentDice.map(side => {
     const newPoints = side.points.map(point => {
-      return new Point(point.x - 50, point.y);
+      return new Point(point.x + xTranslation, point.y + yTranslation);
     });
-    return new Side(newPoints, side.value);
+    return new Side(newPoints, side.dots);
   });
   draw(newDice);
 }
 
-function translateDown() {
-  console.log('translated');
+function scale(xScale, yScale) {
   const newDice = currentDice.map(side => {
     const newPoints = side.points.map(point => {
-      return new Point(point.x, point.y - 50);
+      return new Point(point.x * xScale, point.y * yScale);
     });
-    return new Side(newPoints, side.value);
+    return new Side(newPoints, side.dots);
+  });
+  draw(newDice);
+}
+
+function rotate(angle) {
+  let radians = angle * Math.PI / 180;
+  const newDice = currentDice.map(side => {
+    const newPoints = side.points.map(point => {
+      return new Point((point.x * Math.cos(radians) - point.y * Math.sin(radians)), 
+      (point.x * Math.sin(radians) + point.y * Math.cos(radians)));
+    });
+    return new Side(newPoints, side.dots);
+  });
+  draw(newDice);
+}
+
+function separate(distance) {
+  
+  const newDice = currentDice.map(side => {
+    let pointNumber = -1;
+    const newPoints = side.points.map(point => {
+      pointNumber++;
+      if(point.x == currentDice[0].points[pointNumber].x && point.y > currentDice[0].points[pointNumber].y){
+        return new Point(point.x, point.y + distance); // vai pra cima
+      }
+      if(point.x == currentDice[0].points[pointNumber].x && point.y < currentDice[0].points[pointNumber].y){
+        return new Point(point.x, point.y - distance); // vai pra baixo
+      }
+      if(point.x > currentDice[0].points[pointNumber].x && point.y == currentDice[0].points[pointNumber].y){
+        return new Point(point.x + distance, point.y); // vai pra direita
+      }
+      if(point.x < currentDice[0].points[pointNumber].x && point.y == currentDice[0].points[pointNumber].y){ 
+        return new Point(point.x - distance, point.y); // vai pra esquerda
+      }
+      return new Point(point.x, point.y);
+    });
+    return new Side(newPoints, side.dots);
   });
   draw(newDice);
 }
 
 
-function draw(dice) { // Monta o Dado baseado nos parâmetros enviados
-  var canvas = document.getElementById('cartesian-canvas');
-  var ctx = canvas.getContext('2d');
+function draw(dice) {
+  const canvas = document.getElementById('cartesian-canvas');
+  const ctx = canvas.getContext('2d');
   currentDice = dice;
+  sideSize = Math.abs(dice[0].points[0].x - dice[0].points[2].x)
 
   ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.translate(canvas.width / 2, canvas.height / 2);
   ctx.scale(1, -1);
 
+  drawAxes(ctx);
+
   ctx.strokeStyle = 'black';
+  ctx.fillStyle = 'black';
   ctx.lineWidth = 1;
 
   dice.forEach(side => {
@@ -68,9 +100,111 @@ function draw(dice) { // Monta o Dado baseado nos parâmetros enviados
     for (let i = 1; i < side.points.length; i++) {
       ctx.lineTo(side.points[i].x, side.points[i].y);
     }
-    
     ctx.closePath();
     ctx.stroke();
+    drawDots(ctx, side);
   });
 }
+
+function drawAxes(ctx){
+  ctx.strokeStyle = 'lightgrey';
+  ctx.beginPath();
+  ctx.moveTo(0, -550);
+  ctx.lineTo(0, 550);
+  ctx.moveTo(550, 0);
+  ctx.lineTo(-550, 0);
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function drawDots(ctx, side) {
+  const centerX = (side.points[0].x + side.points[2].x) / 2;
+  const centerY = (side.points[0].y + side.points[2].y) / 2;
+  const dotRadius = Math.abs(side.points[0].x - side.points[2].x) / 20;
+  const offset = Math.abs(side.points[0].x - side.points[2].x) / 5;
+
+  const dotPatterns = {
+    1: () => {
+        drawDot(ctx, centerX, centerY, dotRadius);
+    },
+    2: () => {
+        drawDot(ctx, centerX - offset, centerY + offset, dotRadius);
+        drawDot(ctx, centerX + offset, centerY - offset, dotRadius);
+    },
+    3: () => {
+        drawDot(ctx, centerX - offset, centerY + offset, dotRadius);
+        drawDot(ctx, centerX, centerY, dotRadius);
+        drawDot(ctx, centerX + offset, centerY - offset, dotRadius);
+    },
+    4: () => {
+        drawDot(ctx, centerX - offset, centerY - offset, dotRadius);
+        drawDot(ctx, centerX - offset, centerY + offset, dotRadius);
+        drawDot(ctx, centerX + offset, centerY - offset, dotRadius);
+        drawDot(ctx, centerX + offset, centerY + offset, dotRadius);
+    },
+    5: () => {
+        drawDot(ctx, centerX - offset, centerY - offset, dotRadius);
+        drawDot(ctx, centerX - offset, centerY + offset, dotRadius);
+        drawDot(ctx, centerX, centerY, dotRadius);
+        drawDot(ctx, centerX + offset, centerY - offset, dotRadius);
+        drawDot(ctx, centerX + offset, centerY + offset, dotRadius);
+    },
+    6: () => {
+        drawDot(ctx, centerX - offset, centerY - offset, dotRadius);
+        drawDot(ctx, centerX - offset, centerY, dotRadius);
+        drawDot(ctx, centerX - offset, centerY + offset, dotRadius);
+        drawDot(ctx, centerX + offset, centerY - offset, dotRadius);
+        drawDot(ctx, centerX + offset, centerY, dotRadius);
+        drawDot(ctx, centerX + offset, centerY + offset, dotRadius);
+    }
+  };
+
+    dotPatterns[side.dots]();
+}
+
+function drawDot(ctx, x, y, dotRadius) {
+  ctx.beginPath();
+  ctx.arc(x, y, dotRadius, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function setupButtons(){
+  const translateLeftButton = document.getElementById('translate-left');
+  if (translateLeftButton) {
+    translateLeftButton.addEventListener('click', () => translate(-sideSize/2, 0));
+  }
+  const translateDownButton = document.getElementById('translate-down');
+  if (translateDownButton) {
+    translateDownButton.addEventListener('click', () => translate(0, -sideSize/2));
+  }
+  const translateUpButton = document.getElementById('translate-up');
+  if (translateUpButton) {
+    translateUpButton.addEventListener('click', () => translate(0, sideSize/2));
+  }
+  const translateRightButton = document.getElementById('translate-right');
+  if (translateRightButton) {
+    translateRightButton.addEventListener('click', () => translate(sideSize/2, 0));
+  }
+  const scaleUpButton = document.getElementById('scale-up');
+  if (scaleUpButton) {
+    scaleUpButton.addEventListener('click', () => scale(1.2, 1.2));
+  }
+  const scaleDownButton = document.getElementById('scale-down');
+  if (scaleDownButton) {
+    scaleDownButton.addEventListener('click', () => scale(0.5, 0.5));
+  }
+  const rotateClockwiseButton = document.getElementById('rotate-clockwise');
+  if (rotateClockwiseButton) {
+    rotateClockwiseButton.addEventListener('click', () => rotate(-45));
+  }
+  const rotateAntiClockwiseButton = document.getElementById('rotate-anti-clockwise');
+  if (rotateAntiClockwiseButton) {
+    rotateAntiClockwiseButton.addEventListener('click', () => rotate(45));
+  }
+  const separateButton = document.getElementById('separate');
+  if (separateButton) {
+    separateButton.addEventListener('click', () => separate(sideSize/2));
+  }
+}
+
 
